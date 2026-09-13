1cb9193c23e61b3e54d27abf64087fa8c8c497b1
Jotaro <jotarokujo127@proton.me>
2026-09-13 18:01:47 +0300

    update bootloader to 32 bits

--- bootloader+kernel/bootloader.asm
+++ bootloader+kernel/bootloader.asm
@@ -1,47 +1,97 @@
-[org 0x7c00]
 [bits 16]
+
+section .boot
+global start
 
 start:
     cli
-    mov ax, 0x0000
+    xor ax, ax
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov sp, 0x7c00
-    sti
-    mov ax, 0x0003
+    mov sp, 0x7C00
+
+    mov [boot_drive], dl
+
+
+    mov ax, 0x0013
     int 0x10
-    mov si, msg1
-    call print
-    mov ax, 0x1000
-    mov es, ax
-    xor bx, bx
+
+
     mov ah, 0x02
-    mov al, 40
+    mov al, 16
     mov ch, 0
     mov cl, 2
     mov dh, 0
-    mov dl, 0
+    mov dl, [boot_drive]
+    mov bx, 0x7E00
     int 0x13
-    jc error
-    mov si, msg2
-    call print
-    jmp 0x1000:0x0000
-error:
-    mov si, msg_error
-    call print
-    jmp $
-print:
-    lodsb
-    or al, al
-    jz .done
-    mov ah, 0x0e
-    int 0x10
-    jmp print
-.done:
-    ret
-msg1       db 'Loading kernel...', 0x0d, 0x0a, 0
-msg2       db 'Kernel loaded! Starting...', 0x0d, 0x0a, 0
-msg_error  db 'ERROR!', 0x0d, 0x0a, 0
+    jc disk_error
+
+
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+
+    lgdt [gdt_descriptor]
+
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    jmp CODE_SEG:init_pm32
+
+disk_error:
+    hlt
+    jmp disk_error
+
+[bits 32]
+init_pm32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esp, 0x90000
+
+    extern kernel_main
+    call kernel_main
+
+.halt:
+    hlt
+    jmp .halt
+
+
+gdt_start:
+    dd 0x0, 0x0
+
+gdt_code:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+
+gdt_data:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+boot_drive db 0
 times 510 - ($ - $$) db 0
-dw 0xaa55
+dw 0xAA55
